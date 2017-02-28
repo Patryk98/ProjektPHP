8aa2957728b1828a081aff1a20fd867c9148b3c8
Patryk98 <rechniopatryk@gmail.com>
2017-02-28 08:58:58 +0100

    17

--- dodaj_usera.php
+++ dodaj_usera.php
@@ -1,3 +1,8 @@
 <?php
-  echo "pomyślnie zarejestrowano uzytkownika";
+$imie = $_REQUEST["imie"];
+$nawzwisko = $_REQUEST["nazwisko"];
+$login = $_REQUEST["login"];
+$mail = $_REQUEST["mail"];
+$haslo = $_REQUEST["haslo"];
+
 ?>
